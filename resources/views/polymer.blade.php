a3b05150ec0473732a479af3f522cf17997b51e2
Leomar Bucud <leomarbucud@gmail.com>
2017-05-09 11:49:52 +0400

    Working Layout

--- resources/views/polymer.blade.php
+++ resources/views/polymer.blade.php
@@ -35,11 +35,11 @@
         </grid-app>
         <script type="text/javascript" href="/bower_components/webcomponentsjs/webcomponents.js"></script>
         <script>
-        var gMap = document.querySelector('google-map');
+        // var gMap = document.querySelector('google-map');
 
-        gMap.addEventListener('api-load', function(e) {
-            document.querySelector('google-map-directions').map = this.map;
-        });
+        // gMap.addEventListener('api-load', function(e) {
+        //     document.querySelector('google-map-directions').map = this.map;
+        // });
         </script>
     </body>
 </html>
